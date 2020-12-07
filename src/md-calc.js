cdc592685335e33aef7a5bc7a93979db522da89e
Kyle McCullen <kmccullen@protonmail.com>
2020-12-07 10:52:23 -0500

    cell formatting

--- src/md-calc.js
+++ src/md-calc.js
@@ -1,16 +1,41 @@
 const parser = require('./parser');
 const evaluate = require('./evaluate');
+const cellFormatParser = require('./helpers/cell-format-parser');
+const formatCell = require('./helpers/format-cell');
 
 const mdCalcRegex = /mdCalc[\s\S]*?(?=$|\n\n)/g;
+
+const getCellFormatting = (data) => {
+  const newData = [];
+  const formatting = [];
+
+  data.forEach((row) => {
+    const newDataRow = [];
+    const formattingRow = [];
+    row.forEach((cell) => {
+      const [newCell, format] = cellFormatParser(cell);
+      newDataRow.push(newCell);
+      formattingRow.push(format);
+    });
+    newData.push(newDataRow);
+    formatting.push(formattingRow);
+  });
+
+  return [newData, formatting];
+};
 
 const mdCalc = (text) =>
   text.replace(mdCalcRegex, (match) => {
     const table = parser(match.trim('\n'));
+    const [newData, formats] = getCellFormatting(table.data);
+    table.data = newData;
 
-    const evaluatedData = evaluate(table.data);
+    let evaluatedData = evaluate(table.data);
+    evaluatedData = evaluatedData.map((row, i) =>
+      row.map((cell, j) => formatCell(cell, formats[i][j])));
     table.data = evaluatedData;
-    const rows = [...table.head, ...table.data];
 
+    const rows = [...table.head, ...table.data];
     const newMd = rows.map((row) => `|${row.join('|')}|`).join('\n');
 
     return newMd;
--- src/md-calc.spec.js
+++ src/md-calc.spec.js
@@ -100,4 +100,32 @@ mdCalc
 |Total|1200|`;
     expect(mdCalc(md)).to.equal(newMd);
   });
+
+  it('Should work with cell formatting', () => {
+    const md = `
+mdCalc
+|Home Valuation||
+|-|-|
+|Land Value|[C]=14567.89|
+|Home Value|[C]=39432.11|
+|Current Value|[C]=B1+B2|
+|Purchase Price|[C]=100000|
+|Hove Value %|[P]{2}=B2/B3|
+|Home Value|[C]=B4*B5|
+|Land Value|[C]=B4*(1-B5)|
+|Yearly Deduction|[C]=B6/27.5|`;
+
+    const newMd = `
+|Home Valuation||
+|-|-|
+|Land Value|$14,567.89|
+|Home Value|$39,432.11|
+|Current Value|$54,000.00|
+|Purchase Price|$100,000.00|
+|Hove Value %|73.02%|
+|Home Value|$73,022.43|
+|Land Value|$26,977.57|
+|Yearly Deduction|$2,655.36|`;
+    expect(mdCalc(md)).to.equal(newMd);
+  });
 });
